423d4d0fe424c642fcb5203b7bf289a28a0d92cb
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-09-16 21:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+ var ans = document.getElementById('ans');
+        var input = document.getElementById('input');
+
+        function addNum(value){
+            input.value += ""+value;
+        }
+        function addOp(op){
+            input.value += op;
+        }
+        function handleClear(){
+            input.value = "";
+        }
+        function handleClick(){
+            let res = eval(input.value)
+            input.value = res;
+        }
